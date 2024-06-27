480d70cd406eeaa63da21a72fde78bc52e690d4c
Petko Pedev <petko.pedev@gmail.com>
2024-06-27 22:46:38 +0300

    Create FragileItem.java

---
+++ src/FragileItem.java
@@ -0,0 +1,17 @@
+public class FragileItem extends InventoryItem{
+    private double weight;
+
+    public FragileItem(double weight, int itemId, int quantity, String name, String category, double price, boolean isBreakable, boolean perishable) {
+        super(itemId, quantity, name, category, price, isBreakable, perishable);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double setWeight(double weight) {
+        return this.weight = weight;
+    }
+    
+}
